adceaf9bd5558de756dfb23554aa2cf95fa94ad4
oliver-turner <oliver01turner@gmail.com>
2023-12-14 16:12:40 +1100

    first commit

---
+++ read.py
@@ -0,0 +1,15 @@
+from PiicoDev_RFID import PiicoDev_RFID
+from PiicoDev_Unified import sleep_ms
+
+rfid = PiicoDev_RFID()
+
+print('Place tag near the RFID Module')
+
+while True:
+    if rfid.tagPresent():
+        id = rfid.readID()
+        id = rfid.readID(detail=True)
+
+        print(id)
+
+    sleep_ms(100)
