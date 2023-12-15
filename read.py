61f228ce0df2b30c5ba58756c7719f309c992ed6
oliver-turner <oliver01turner@gmail.com>
2023-12-15 19:45:28 +1100

    completed functionality of rfid reader

--- read.py
+++ read.py
@@ -5,7 +5,6 @@ from spotipy.oauth2 import SpotifyOAuth
 from PiicoDev_RFID import PiicoDev_RFID
 from PiicoDev_Unified import sleep_ms
 
-
 DEVICE_ID=os.getenv('MY_DEVICE_ID')
 CLIENT_ID=os.getenv('MY_CLIENT_ID')
 CLIENT_SECRET=os.getenv('MY_CLIENT_SECRET')
@@ -16,16 +15,18 @@ while True:
         spotifyAuth = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
                                                         client_secret=CLIENT_SECRET,
                                                         redirect_uri="https://google.com/",
-                                                        scope="user-read-playback-state, user-modify-playback-state, open_browser=False"))
+                                                        scope="user-read-playback-state,user-modify-playback-state",
+                                                        open_browser=False))
+
+        print("Please scan a record you want to listen to :)")
 
         while True:
             if rfid.tagPresent():
-                print("Please scan a record to listen to :)")
                 id = rfid.readID()
                 print("You have scanned: ", id)
                 spotifyAuth.transfer_playback(device_id=DEVICE_ID, force_play=False)
-                if (id=='04:26:85:FA:3F:74:81'):
-                    sp.start_playback(device_id=DEVICE_ID, uris=['spotify:track:4GKphhKJprqrPkLuU8Vsyu'])
+                if (id=="04:26:85:FA:3F:74:80"):
+                    spotifyAuth.start_playback(device_id=DEVICE_ID, uris=['spotify:track:4GKphhKJprqrPkLuU8Vsyu'])
                     sleep_ms(100)
 
     except Exception as e:
