ea2699388f9ebe9967905fca96baf84fb3511c60
oliver-turner <oliver01turner@gmail.com>
2023-12-15 18:15:09 +1100

    commenting out to rewrite functionality

--- read.py
+++ read.py
@@ -3,13 +3,13 @@ from PiicoDev_Unified import sleep_ms
 
 rfid = PiicoDev_RFID()
 
-print('Place tag near the RFID Module')
-
-while True:
-    if rfid.tagPresent():
-        id = rfid.readID()
-        id = rfid.readID(detail=True)
-
-        print(id)
-
-    sleep_ms(100)
+#print('Place tag near the RFID Module')
+#
+#while True:
+#    if rfid.tagPresent():
+#       id = rfid.readID()
+#       id = rfid.readID(detail=True)
+#
+#       print(id)
+#
+#    sleep_ms(100)
